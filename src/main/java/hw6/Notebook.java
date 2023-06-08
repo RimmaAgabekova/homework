52815e390a5769cbd14ae788fff299efa99038f2
Rimma <mudalieva@yandex.ru>
2023-06-08 19:33:52 +0400

    решение дом.работы для семинара №6

---
+++ src/main/java/hw6/Notebook.java
@@ -0,0 +1,45 @@
+package hw6;
+
+import java.util.Objects;
+
+public class Notebook {
+         String brand;
+        String colour;
+    public Notebook(String brand, String colour) {
+        this.brand = brand;
+        this.colour = colour;
+
+    }
+    public String getBrand() {
+        return this.brand;
+    }
+    public String getColour() {
+        return this.colour;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "notebook{" + "brand='" + brand + '\'' +
+                ", colour='" + colour + '\'' +
+
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notebook notebook = (Notebook) o;
+        return Objects.equals(brand, notebook.brand) &&
+                Objects.equals(colour, notebook.colour);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, colour);
+    }
+}
